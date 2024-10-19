085ab87cd72f92e01b5257dca77b2e6787ab1281
cloudawsauto <santoshaavula16@gmail.com>
2024-10-19 21:14:09 +0530

    dev commit

--- scripts/main.tf
+++ scripts/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_s3_bucket" "spacelift-test1-s3" {
-   bucket = "spacelift-test1-s3" 
+resource "aws_s3_bucket" "dev-infra-s3" {
+   bucket = "dev-infra-s3" 
 }
