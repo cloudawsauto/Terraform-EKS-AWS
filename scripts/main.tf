754691dd19a7656129c5e6ae7ff0aa5e1af1bea8
cloudawsauto <santoshaavula16@gmail.com>
2024-10-22 22:33:25 +0530

    VPC commit

--- scripts/main.tf
+++ scripts/main.tf
@@ -1,6 +1,73 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 resource "aws_s3_bucket" "dev-infra-s3" {
    bucket = "dev-infra-s3" 
 }
+resource "aws_vpc" "STG_VPC" {
+  cidr_block = var.stg_cidr
+}
+resource "aws_subnet" "pub_subnet1" {
+  vpc_id = aws_vpc.STG_VPC.id
+  availability_zone = "us-east-1a"
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    env = STG
+  }
+  }
+  resource "aws_subnet" "pub_subnet2" {
+  vpc_id = aws_vpc.STG_VPC.id
+  availability_zone = "us-east-1b"
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    env = STG
+  }
+  }
+  resource "aws_subnet" "prv_subnet1" {
+    vpc_id = aws_vpc.STG_VPC.id
+    availability_zone = "us-east-1c"
+    cidr_block = "10.0.3.0/24"
+    map_public_ip_on_launch = false
+    tags = {
+      env= STG_prv
+    }
+  }
+  resource "aws_subnet" "prv_subnet2" {
+    vpc_id = aws_vpc.STG_VPC.id
+    availability_zone = "us-east-1d"
+    cidr_block = "10.0.4.0/24"
+    map_public_ip_on_launch = false
+    tags = {
+      env= STG_prv
+    }
+  }
+  resource "aws_route_table" "pub_RT" {
+    vpc_id = aws_vpc.STG_VPC.id
+
+    route = {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.my_igw.id
+      }
+  }
+  resource "aws_route_table" "prv_RT" {
+    vpc_id = aws_vpc.STG_VPC.id
+    route = {
+      cidr_block = "0.0.0.0/0"
+    }
+  }
+
+  resource "aws_internet_gateway" "my_igw" {
+    vpc_id = aws_vpc.STG_VPC.id
+  }
+
+  resource "aws_route_table_association" "pub_RTAS" {
+    subnet_id = [aws_subnet.pub_subnet1.id, aws_subnet.pub_subnet2.id]
+    route_table_id = aws_route_table.pub_RT.id
+  }
+
+  resource "aws_route_table_association" "prv_RTAS" {
+    subnet_id = [aws_subnet.prv_subnet1.id, aws_subnet.prv_subnet2.id]
+    route_table_id = aws_route_table.prv_RT.id
+  }
---
+++ scripts/variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  description = "region"
+  type        = string
+  default = "us-east-1"
+}
