27c07c50f1025dc704d2ac63e00c4ebe89f3d97c
cloudawsauto <santoshaavula16@gmail.com>
2024-10-23 12:27:17 +0530

    cidr error commit

--- scripts/variables.tf
+++ scripts/variables.tf
@@ -3,3 +3,18 @@ variable "region" {
   type        = string
   default = "us-east-1"
 }
+
+variable "ami_id" {
+  description = "ami_id"
+  type = string
+  default = "ami-0261755bbcb8c4a84"
+  }
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+variable "stg_cidr" {
+  type = string
+  default = "10.0.0.0/16"
+}
